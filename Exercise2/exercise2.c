24f0a2296a8cf3577746658a3c1ad18f99992ad4
ARAtprical <55189113+ARAtprical@users.noreply.github.com>
2019-09-11 18:07:32 +0200

    Revert "Version 1"
    This reverts commit 8e8840fc8c47ba51d4d745645570c849fe049d3f.

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,12 +1,5 @@
 /* Here, you should entry the required libraries */
-#include<stdio.h>
+
 void main(){
 	/* Here, you should write the source code of your program */
-	int a,b,c;
-	printf("Please entry the first number:");
-	scanf("%d",&a);
-	printf("Please entry the second number:");
-	scanf("%d",&b);
-	c=a+b;
-	printf("The sum of both numbers is: %d",&c);
 }
